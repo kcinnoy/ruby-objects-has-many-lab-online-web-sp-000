f04daa310164444e45c67725a8f81c2f90586eee
Yonnick Brown <25hour24@gmail.com>
2019-02-16 20:04:08 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,28 +1,22 @@
-
-class Song
-  attr_accessor :title, :artist
+class Post
+  attr_accessor :title, :author
   @@all = []
-
+  
   def initialize(title)
     @title = title
     @@all << self
   end
-
-  def self.all
-    @@all
+  
+  def self.all 
+    @@all 
   end
-
-  def name
-    @title
+  
+  def author_name
+    if self.author == nil
+      nil 
+    else 
+      self.author.name 
+    end
   end
-
-
-  def artist_name
-    if self.artist == nil
-        nil
-  else
-    self.artist.name
-  end
-  end
-
+  
 end
